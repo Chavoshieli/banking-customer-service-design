8e4aa2de88cd8c7ec88e6f590e69cc35306842ef
Chavoshieli <37370923+Chavoshieli@users.noreply.github.com>
2026-05-26 10:33:59 +0330

    Update customer_profile_query.sql

--- sql/customer_profile_query.sql
+++ sql/customer_profile_query.sql
@@ -1,1 +1,36 @@
+-- =====================================================
+-- Customer Profile Query
+-- Banking Customer Information Service
+-- Database: DB2
+-- Description:
+-- Retrieves customer identity and job information
+-- Supports optional secondary job records
+-- =====================================================
 
+SELECT
+    c.intcustid,
+
+    hc.nationalcode,
+
+    CASE
+        WHEN c.BusinessAcc = 1 THEN 'Yes'
+        ELSE 'No'
+    END AS BusinessAccount,
+
+    j1.jobname AS primary_job,
+
+    j2.jobname AS secondary_job
+
+FROM customers c
+
+INNER JOIN hcustomers hc
+        ON hc.intcustid = c.intcustid
+
+LEFT JOIN hecustomers j1
+       ON j1.intcustid = c.intcustid
+      AND j1.jobtype = 1
+
+LEFT JOIN hecustomers j2
+       ON j2.intcustid = c.intcustid
+      AND j2.jobtype = 2
+;
